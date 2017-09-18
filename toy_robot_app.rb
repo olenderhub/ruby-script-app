bd75ba1227d93909d327e3827d7eaa116e70fa2e
Hubert Olender <olender.hub@gmail.com>
2017-09-18 17:00:09 +0200

    Connect all classes together in toy robot app

--- toy_robot_app.rb
+++ toy_robot_app.rb
@@ -1,2 +1,18 @@
-puts 'Welcome to Toy Robot Application! All available commands you can find in readme.'
+Dir["./lib/*.rb"].each                {|file| require file }
+Dir["./lib/robot_commands/*.rb"].each {|file| require file }
 
+table       = Table.new(5, 5)
+robot       = Robot.new
+interpreter = Interpreter.new(robot, table)
+
+puts 'Welcome to Toy Robot Application!'
+
+loop do
+  puts 'Please type your command'
+  input_command = STDIN.gets&.chomp
+
+  break if input_command == 'EXIT' || input_command.nil?
+
+  called_input_command = interpreter.call(input_command)
+  puts called_input_command.output if input_command == 'REPORT' && called_input_command
+end
